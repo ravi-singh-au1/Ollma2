4d825fa0a94ece8fbe87952cbf8698e2687fd2e7
ravi singh <42317356+ravi-singh-au1@users.noreply.github.com>
2025-04-14 15:13:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from langchain_community.llms import Ollama
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
+
 load_dotenv()
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 
 ## langsmith tracking
 os.environ['LANGCHAIN_API_KEY']= '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'
 os.environ['LANGCHAIN_TRACING_V2']='True'
-os.environ['LANGCHAIN_PROJECT']='GenAIAPPWithOPENAI'
+os.environ['LANGCHAIN_PROJECT']=os.getenv('LANGCHAIN_PROJECT')
 
 promt = ChatPromptTemplate.from_messages(
 [
@@ -25,15 +25,8 @@ st.title("Lanchain Demo with Gemma Model")
 input_text = st.text_input("What question you have in mind?")
 
 ## Ollma Llama2
-llm= ChatOpenAI(model='gpt-4o')
+llm= Ollama(model='gemma:2b')
 output_parser = StrOutputParser()
 chain = promt|llm|output_parser
-import streamlit as st
-from requests.exceptions import ConnectionError
-
-if chain.invoke({'question': input_text}):
-    response = chain.invoke({'question': input_text})
-    st.write(response)
-
-
-
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
